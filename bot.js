13d963f92caaa3b29817745a11ef4918a002a16f
Ruyan Chen <colortheskyjade@users.noreply.github.com>
2019-11-03 21:36:15 -0800

    i got ahead of myself XD

---
+++ bot.js
@@ -0,0 +1,48 @@
+const auth = require('./auth.json');
+// LOL :C
+// const lib = require('./lib.js');
+const { Client, RichEmbed } = require('discord.js');
+
+const bot = new Client();
+
+// sorry dude
+const react_to_me = '173523956173766656';
+const booty_emote = '445820252245196810';
+// const react_to_me = '49395063955914752';
+
+bot.on('ready', () => {
+  console.log(`[INFO] Logged in as ${bot.user.tag}.`);
+});
+
+bot.on('disconected', () => {
+  console.log(
+    '[INFO] Disconnected from API service, attempted to reconnect...'
+  );
+});
+
+bot.on('warn', msg => {
+  console.log('[WARN] ' + msg);
+});
+
+bot.on('error', function(err) {
+  console.log('[ERROR] ' + err.message);
+  process.exit(1);
+});
+
+bot.on('message', msg => {
+  // NOTE: consider checking msg.author.id === bot.user.id for scope
+  if (msg.author.bot) return;
+
+  // TODO: refactor, this will be changed in v12
+  if (msg.isMemberMentioned(msg.guild.member(bot.users.get(react_to_me)))) {
+    msg
+      .react('640745183238946867')
+      .then(() => msg.react('🍽'))
+      .catch(() =>
+        // TODO: possibly clear reactions
+        console.log('[ERROR] Failed to react properly with emojis.')
+      );
+  }
+});
+
+bot.login(auth.token);
